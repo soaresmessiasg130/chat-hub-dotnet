884bb6a708cf8f4d13792be59bbf6399c70b24fe
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2024-09-15 14:34:19 -0300

    add(client): simple and functional client consuming server;

---
+++ src/ChatHub/ChatHub.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var uri = "http://localhost:5287/chat";
+
+await using var connection = new HubConnectionBuilder()
+  .WithUrl(uri)
+  .Build();
+
+await connection.StartAsync();
+
+await foreach (var date in connection.StreamAsync<DateTime>("Streaming"))
+{
+  Console.WriteLine(date);
+}
