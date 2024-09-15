3f28223d2fe72712731ecae1f72e2111e3f5740a
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2024-09-15 17:22:55 -0300

    refact(server): add interface to GeneralChatHub;

--- src/ChatHub/ChatHub.Server/Hubs/GeneralChatHub.cs
+++ src/ChatHub/ChatHub.Server/Hubs/GeneralChatHub.cs
@@ -1,8 +1,20 @@
 using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.SignalR;
 
-class GeneralChatHub : Hub
+namespace ChatHub.Server.Hubs;
+
+public class GeneralChatHub : Hub<IGeneralChatHub>
 {
+  public string GetConnectionId()
+  {
+    return Context.ConnectionId;
+  }
+
+  public override async Task OnConnectedAsync()
+  {
+    await Clients.All.SendMessageToAllAsync($"New connection, thanks: {Context.ConnectionId}");
+  }
+
   public async IAsyncEnumerable<DateTime> Streaming([EnumeratorCancellation] CancellationToken cancellationToken)
   {
     while (true)
@@ -12,4 +24,9 @@ class GeneralChatHub : Hub
       await Task.Delay(1000, cancellationToken);
     }
   }
+}
+
+public interface IGeneralChatHub
+{
+  Task SendMessageToAllAsync(string message);
 }
