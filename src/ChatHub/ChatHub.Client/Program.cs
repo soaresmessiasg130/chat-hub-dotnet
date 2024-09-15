4a749a84177e18b788957c62f8e74446bcb9aa75
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2024-09-15 17:21:11 -0300

    refact(client): refact client code;

--- src/ChatHub/ChatHub.Client/Program.cs
+++ src/ChatHub/ChatHub.Client/Program.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.SignalR.Client;
 
-var uri = "http://localhost:5287/chat";
+var uri = "http://localhost:5287/generalchathub";
 
 await using var connection = new HubConnectionBuilder()
   .WithUrl(uri)
   .Build();
 
 await connection.StartAsync();
+
+connection.On<string>("SendMessageToAllAsync", s =>
+{
+  Console.WriteLine($"My ID: {connection.ConnectionId}");
+  Console.WriteLine($"Notification: {s}");
+});
 
 await foreach (var date in connection.StreamAsync<DateTime>("Streaming"))
 {
